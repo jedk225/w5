c86511dec88b56000265b58146910e46885b926d
Muddy Lee <muddy@Muddys-iMac.local>
2018-11-12 11:59:49 -0800

    barebones structure for three pages.  Landing page, setup Project, and Project.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch  } from "react-router-dom"
 import logo from './logo.svg';
+import landingPage from "../src/pages/landingPage"
+import setupProject from "../src/pages/setupProject"
+import project from "../src/pages/project"
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={landingPage} />
+          <Route exact path="/project" component={setupProject} />
+          <Route exact path="/project/:id" component={project} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
-    );
-  }
-}
+    </Router>
+)
 
 export default App;
+
