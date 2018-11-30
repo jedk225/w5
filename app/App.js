b3e3920b535f9706282e96e6344922e145e925af
fernando_R <fer0624@gmail.com>
2018-11-29 21:33:45 -0800

    roolback

--- app/App.js
+++ app/App.js
@@ -62,10 +62,10 @@ export default class App extends Component {
     errorMessage: null,
   };
 
- handleSubmit = () => {
+ handleSubmit = (comments,photo) => {
  //Api.submitForm(this.state.location);
 
- Api.saveLocation(this.state.location);
+ Api.saveLocation(this.state.location, comments, photo, "fx@gmail.com", "ufo spotter");
 
 
   }
@@ -141,7 +141,7 @@ export default class App extends Component {
         )}
         <View style={{ height: 50 }}>
 
-          <Button title="submit location"
+          <Button title="UFO SPOTTER"
             onPress={this.handleSubmit}
 
           />
@@ -150,7 +150,9 @@ export default class App extends Component {
 
         </View>
         <View style={{ height: -200 }}>
-        <Inputs />
+        <Inputs handleSubmit={
+          this.handleSubmit
+        } />
         </View>
 
 
@@ -166,16 +168,32 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 23
     //alignItems: 'center',
     //justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    //paddingTop: Constants.statusBarHeight,
+    //backgroundColor: '#ecf0f1',
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
     textAlign: 'center',
   },
+ input: {
+    margin: 15,
+    height: 40,
+    borderColor: '#7a42f4',
+    borderWidth: 1
+ },
+ submitButton: {
+    backgroundColor: '#7a42f4',
+    padding: 10,
+    margin: 15,
+    height: 40,
+ },
+ submitButtonText:{
+    color: 'white'
+ }
 });
 
 
